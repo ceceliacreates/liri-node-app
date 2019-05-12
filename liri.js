a3bcd4697e74eaadf7f648f6725294de32c588e9
ceceliacreates <40367173+ceceliacreates@users.noreply.github.com>
2019-05-12 16:48:42 -0400

    adds switch case for command logic and installs packages

--- liri.js
+++ liri.js
@@ -1,3 +1,23 @@
+const Spotify = require('node-spotify-api');
 require("dotenv").config();
-var keys = require("./keys.js");
-var spotify = new Spotify(keys.spotify)
+const keys = require("./keys.js");
+const spotify = new Spotify(keys.spotify)
+const fs = require("fs")
+const axios = require("axios")
+const moment = require('moment');
+
+const command = process.argv[2];
+
+switch (command) {
+    case "concert-this":
+      console.log("concert-this");
+      break;
+    case "spotify-this-song":
+      console.log("spotify-this-song");
+      break;
+    case "movie-this":
+      console.log("movie-this");
+      break;
+    case "do-what-it-says":
+      console.log("do-what-it-says");
+  }
